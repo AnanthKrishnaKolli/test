70ff63b1da32425fbaec16b9c48425562b381ca1
AnanthKrishnaKolli <ananth.36.krishna@gmail.com>
2023-09-11 11:41:57 +0530

    Added bad examples of code

---
+++ testlistcomprehension.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+new_list_numbers = []
+
+for i in numbers:
+
+    new_list_numbers.append(i * 2)
---
+++ testlongifelse.py
@@ -0,0 +1,17 @@
+x = 10
+
+value = input("Enter a number: ")
+
+y = int(value)
+
+if x < y:
+
+    print(f"{x} is less than {y}")
+
+elif x == y:
+
+    print(f"{x} is equal to {y}")
+
+else:
+
+    print(f"{x} is more than {y}")
